4e418b847de4bab3f5e8c27a7d5b20d1035e7b17
Diego Alejandro Marin Soto <diegomarin1915@gmail.com>
2022-04-01 20:19:18 -0500

    Improve countries CRUD

---
+++ TiendaOnline.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaOnline.Web.Models;
+
+
+
+namespace TiendaOnline.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+
+
+        public DbSet<Country> Countries { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+
+
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+    }
+}
---
+++ TiendaOnline/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaOnline.Models
+{
+    public class Country { 
+        public int Id { get; set; } 
+        [MaxLength(50, ErrorMessage = "El campo {0} debe contener al menos un caracter")]
+        [Required] 
+        public string Name { get; set; } 
+    }
+}
